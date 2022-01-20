4548947538aa8fc9e13a90b95bd2064c7d498af9
Ajay Kathait <kathaitjayk@gmail.com>
2022-01-20 11:16:25 +0530

    import sidebar

--- pages/foxChat/[id].js
+++ pages/foxChat/[id].js
@@ -1,5 +1,5 @@
 import React from "react";
-import Sidebar from "../../components/sidebar";
+import Sidebar from "../../components/Sidebar"
 import ChatScreen from "../../components/ChatScreen";
 import { async } from "@firebase/util";
 import { db } from "../../firebase";
